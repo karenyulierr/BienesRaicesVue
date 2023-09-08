bd4662f17660ce7617afc90bd42bb790fecdca9a
Karen Rodriguez <kyrodriguez@gmail.com>
2023-09-08 14:59:50 -0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,86 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { onAuthStateChanged } from 'firebase/auth';
+import { useFirebaseAuth } from 'vuefire';
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/admin/AdminLayout.vue'),
+      meta: { requiresAuth: true },
+
+      children: [
+        {
+          path: '/admin/propiedades',
+          name: 'admin-propiedades',
+          component: () => import('../views/admin/AdminView.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/admin/nueva',
+          name: 'nueva-propiedad',
+          component: () => import('../views/admin/NuevaPropiedadView.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/admin/editar/:id',
+          name: 'editar-propiedad',
+          component: () => import('../views/admin/EditarPropiedadView.vue'),
+          meta: { requiresAuth: true },
+        },
+      ]
+    },
+
+  ]
+})
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(url => url.meta.requiresAuth);
+
+  if (requiresAuth) {
+    //comprobar que el usuario este autenticado
+    try {
+      await authenticateUser()
+      next();
+    } catch (error) {
+      console.log(error)
+      next({ name: 'login' });
+    }
+  } else {
+    //NO esta protegido
+    next();
+  }
+
+})
+
+function authenticateUser() {
+
+  const auth = useFirebaseAuth();
+
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      if (user) {
+        resolve(user);
+      } else {
+        reject();
+      }
+    })
+  });
+}
+
+//Guard de navegacion
+export default router
